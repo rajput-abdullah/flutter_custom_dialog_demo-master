c0ca7c9ecaffe15ddf8ac417e84d1f9f7925b6f9
Abdullah <abdullahrajput880@gmail.com>
2021-02-18 17:44:01 +0500

    Initial commit

---
+++ lib/dialogs.dart
@@ -0,0 +1,43 @@
+import 'package:custom_dialog_flutter_demo/custom_dialog_box.dart';
+import 'package:flutter/material.dart';
+
+class Dialogs extends StatefulWidget {
+  @override
+  _DialogsState createState() => _DialogsState();
+}
+
+class _DialogsState extends State<Dialogs> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Custom Dialog In Flutter"),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+      ),
+      body: Container(
+        child: Center(
+          child: TextField(
+            readOnly: true,
+            decoration: InputDecoration(
+              hintText:('Select User'),
+              hintStyle: TextStyle(fontSize: 20.0, color: Colors.redAccent),
+            ),
+              onTap: (){
+                showDialog(context: context,
+                  builder: (BuildContext context){
+                  return CustomDialogBox(
+                    title: "Custom Dialog Demo",
+                    descriptions: "Hii all this is a custom dialog in flutter and  you will be use in your flutter applications",
+                    text: "Yes",
+                  );
+                  }
+                );
+              },
+
+          ),
+        ),
+      ),
+    );
+  }
+}
